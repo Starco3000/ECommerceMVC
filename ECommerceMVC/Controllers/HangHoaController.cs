acdf9b976c5bde1d574f722d7dfa68af15b6cc40
Staco3000 <minhnguyen9686q6@gmail.com>
2024-08-22 17:12:12 +0700

    Product List Layout & Search feature

--- ECommerceMVC/Controllers/HangHoaController.cs
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly Hshop2023Context db;
+
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
+
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MotaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MotaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
         }
     }
 }
---
+++ ECommerceMVC/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MotaNgan { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
